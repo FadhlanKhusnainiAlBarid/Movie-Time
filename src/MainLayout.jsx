79b80e1cc4cb2e250dbd181e4b85bd0106b8851c
aXLan666 <angaragilang266@gmail.com>
2025-04-25 12:46:56 +0700

    chore: update

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./components/sidebar";
 import Navbar from "./components/navbar";
@@ -6,13 +6,16 @@ import Navbar from "./components/navbar";
 
 export default function MainLayout() {
   // const { newUrlReq } = useAddFavorite();
+  const containerGen = useRef();
+
+  containerGen.current.eventListenner()
 
   // useEffect(() => {
   //   console.log(newUrlReq);
   // }, []);
   return (
     <div className="relative container mx-auto h-min">
-      <Sidebar />
+      <Sidebar ref={containerGen} />
 
       <Navbar />
 
